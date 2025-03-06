4721a767c3bc7c67c2cd0d4242e8b25ee3aa47aa
blooraayy <jaime.alv.fdez@gmail.com>
2025-03-06 17:06:13 +0100

    datatypes

---
+++ Basics/02-datatypes.js
@@ -0,0 +1,26 @@
+// String (cadena de texto)
+let name = "Jaime"; // comillas dobles
+let alias = 'BlooRay' // comillas simples
+let email = `jaime.alv.fdez@gmail.com` // comillas invertidas
+
+// Number (número)
+let age = 19; // número entero
+let height = 1.82; // número decimal
+
+// Boolean (verdadero o falso)
+let isAlive = true; // verdadero
+let isDead = false; // falso
+
+// Undefined (indefinido)
+let x; // variable sin valor asignado
+console.log(x); // undefined
+
+// Null (nulo)
+let y = null; // valor nulo
+console.log(y); // null
+
+// Symbol (símbolo)
+let sym = Symbol('mySymbol') // símbolo
+console.log(sym);
+
+// BigInt (número entero grande)
