a60ea94f9de78b364beb04dbcdfc058eba6b6442
blooraayy <jaime.alv.fdez@gmail.com>
2025-03-21 19:51:16 +0100

    Operadores Aritméticos

---
+++ src/01_tipos/04-operators.js
@@ -0,0 +1,37 @@
+let a = 2;
+let b = 4;
+// Operadores Aritméticos (Usados para realizar operaciones)
+
+// Suma
+let sum = a + b; // 6
+console.log(sum);
+// Resta
+let sub = a - b; // -2
+console.log(sub);
+// Multiplicación
+let mult = a * b; // 8
+console.log(mult);
+// División
+let div = a / b; // 0.5
+console.log(div);
+// Módulo(resto)
+let mod = a % b; // 2
+console.log(mod);
+// Exponente
+let exp =  a ** b; // 16
+console.log(exp);
+// Incremento
+let post_inc = a++; // 2 (suma 1, pero después de mostrar por pantalla, post-incremento)
+let pre_inc = ++b // 5 (suma 1, antes de mostrar, pre-incremento)
+console.log(post_inc);
+console.log(pre_inc);
+// Decremento
+let post_dec = a--; // 3 (resta 1, pero después de mostrar por pantalla, post-decremento)
+let pre_dec = --b; // 4 (resta 1, pero antes de mostrar, pre-decremento)
+console.log(post_dec);
+console.log(pre_dec);
+
+// Operadores de Asignación (usados para asignar valores a variables)
+
+
+// Operadores de Comparación (Usados para comparar valores y devuelven un booleano)
